d815b9d716ce4571e2cb7ba23e89e306619e7539
bejnoo <bunyamingulten@gmail.com>
2022-07-10 16:28:28 -0400

    First commit

--- Test/src/Test1.java
+++ Test/src/Test1.java
@@ -1,5 +1,8 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello word");
+        System.out.println("Hello word");
+        System.out.println("Hello word");
+        System.out.println("Hello word");
     }
 }
