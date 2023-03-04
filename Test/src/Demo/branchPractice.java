6858ed3ecab714dd27ba0768f82e433faf78b4c9
bejnoo <bunyamingulten@gmail.com>
2023-03-04 17:57:13 -0500

    last one

--- Test/src/Demo/branchPractice.java
+++ Test/src/Demo/branchPractice.java
@@ -2,29 +2,29 @@ package Demo;
 
 public class branchPractice {
     public static void main(String[] args) {
-//        System.out.println("goodbye world");
+        System.out.println("goodbye world");
 
-//        String[] input = {"This", "is", "Java", "Program"};
-//        String[] output = new String[input.length];
-//        for(int i=0;i<=input.length-1;i++){
-//            String revTem="";
-//            for (int j=input[i].length()-1;j>=0;j--){
-//                revTem+=input[i].charAt(j);
-//            }
-//            output[i]=revTem;
-//        }
-//        for (int i=0;i<=output.length-1;i++){
-//            System.out.print(output[i]+" ");
-//        }
+        String[] input = {"This", "is", "Java", "Program"};
+        String[] output = new String[input.length];
+        for(int i=0;i<=input.length-1;i++){
+            String revTem="";
+            for (int j=input[i].length()-1;j>=0;j--){
+                revTem+=input[i].charAt(j);
+            }
+            output[i]=revTem;
+        }
+        for (int i=0;i<=output.length-1;i++){
+            System.out.print(output[i]+" ");
+        }
 
-//        int i = 0;
-//        String revTem = "";
-//        for (int j = input[i].length() - 1; j >= 0; j--) {
-//            revTem += input[i].charAt(j);.
-//
-//            for (int i = 0; i <= output.length - 1; i++) {
-//            System.out.print(output[i]+" ");
-//            }
-//        }
+        int i = 0;
+        String revTem = "";
+        for (int j = input[i].length() - 1; j >= 0; j--) {
+            revTem += input[i].charAt(j);.
+
+            for (int i = 0; i <= output.length - 1; i++) {
+            System.out.print(output[i]+" ");
+            }
+        }
     }
 }
